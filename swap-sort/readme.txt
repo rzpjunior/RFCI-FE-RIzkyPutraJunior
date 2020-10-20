64a7ff730eb66709f7b2708e905ceab7f5c15da9
rzpjunior <rizkyputrajunior@gmail.com>
2020-10-20 23:14:12 +0700

    initial

---
+++ swap-sort/readme.txt
@@ -0,0 +1,4 @@
+write bubblesort in console 
+
+example :
+bubblesort([4, 7, 9, 8, 3, 5])
--- swap-sort/swap-sort.js
+++ swap-sort/swap-sort.js
@@ -1,20 +1,35 @@
-function bubblesort(array) {
+function bubblesort(array,first = true) {
+  // initaial variable
   len = array.length;
+  var i = 0;
+  var j = 1;
+  var totalSwap = 0;
 
-  for (let i = 0; i < len; i++) {
-      for (let j = 0; j < len - i; j++) {
-          let a = array[j];
-          if (a != array[-1]) {
-              var b = array[j + 1];
-              if (a > b) {
-                  array[j] = b;
-                  array[j + 1] = a;
-              }
-          }
+  for (var i = 0; i < len; i++) {
+    // a condition to swap array
+    if (array[i] > array[j]) {
+      var arrayI = array[i];
+      var arrayJ = array[j];
+      array[i] = array[j];
+      array[j] = arrayI;
+
+      totalSwap = totalSwap + 1;
+      console.log('['+ arrayI +','+ arrayJ +'] => ',array);
+      // call this method again to start from array 0
+      var sort = bubblesort(array,false);
+
+      // a condition to let know that the array is sorted
+      if (sort.isEnd == 1) {
+        // if we back again to first method we return total swap
+        if (first == true) { return 'Jumlah Swap = ' + sort.totalSwap }
+        return  { isEnd: 1,totalSwap: sort.totalSwap + 1 };
       }
+    }
+
+    // for last method give return to over looping
+    if (i == (len - 1)) {
+      return { isEnd: 1,totalSwap: totalSwap + 1 };
+    }
+    j++;
   }
 }
-
-let array = [4, 9, 7, 5, 8, 9, 3];
-bubblesort(array);
-console.log(array)
